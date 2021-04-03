b223e925d7aa7d71dbfdfd4343e3854b698b90ce
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-04 02:02:40 +0300

    Тестирование добавления subdocuments к существующему пользователю

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -12,4 +12,21 @@ describe("Subdocuments", () => {
         done();
       });
   });
+
+  it("can add subdocuments to an existing record", (done) => {
+    const joe = new User({ name: "Joe", posts: [] });
+
+    joe
+      .save()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        user.posts.push({ title: "New Post" });
+        return user.save();
+      })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user.posts[0].title === "New Post");
+        done();
+      });
+  });
 });
