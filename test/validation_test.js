1b5d958cd63f7f0e7ff72909f9aada9111944783
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-03 22:07:20 +0300

    Добавила валидацию длины поля name

--- test/validation_test.js
+++ test/validation_test.js
@@ -8,4 +8,11 @@ describe("Validating records", () => {
     const { message } = validationResult.errors.name;
     assert(message === "Name is required.");
   });
+
+  it("requires a user's name longer then 2 characters", () => {
+    const user = new User({ name: "Al" });
+    const validationResult = user.validateSync();
+    const { message } = validationResult.errors.name;
+    assert(message === "Name must be longer then 2 characters.");
+  });
 });
