815801a942e34f686eb2bf02090ccb262179f4cc
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-04 00:53:00 +0300

    Добавила PostSchema и свойство post в модель User

--- src/user.js
+++ src/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const PostSchema = require("./post");
 
 const UserSchema = new Schema({
   name: {
@@ -11,6 +12,7 @@ const UserSchema = new Schema({
     required: [true, "Name is required."],
   },
   postCount: Number,
+  post: [PostSchema],
 });
 
 const User = mongoose.model("user", UserSchema);
