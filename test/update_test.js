4978b602932c1c2a4dc4b6fbefb73ded19a36c90
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-03 21:16:32 +0300

    Тестирование апдейта с инкрементом

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Update records", () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -52,5 +52,14 @@ describe("Update records", () => {
       ),
       done
     );
+  });
+
+  it("A user can have their postCount incremented by 1", (done) => {
+    User.updateMany({ name: "Joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
