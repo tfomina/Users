2c6600644114f6f097948be3697c851966aa2115
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-07 22:46:29 +0300

    В модели User сделала свойство postCount виртуальным, добавила тестирование виртуального свойства, исправила тестирование инкремента

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Update records", () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: "Joe", postCount: 0 });
+    joe = new User({ name: "Joe", likes: 0 });
     joe.save().then(() => done());
   });
 
@@ -54,11 +54,11 @@ describe("Update records", () => {
     );
   });
 
-  it("A user can have their postCount incremented by 1", (done) => {
-    User.updateMany({ name: "Joe" }, { $inc: { postCount: 1 } })
+  it("A user can have their likes incremented by 1", (done) => {
+    User.updateMany({ name: "Joe" }, { $inc: { likes: 1 } })
       .then(() => User.findOne({ name: "Joe" }))
       .then((user) => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       });
   });
