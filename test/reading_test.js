b831bf8916f591cdd42a45c41e4a2c074c2a92a9
Tatyana Fomina <tatyana.fomina.1986@gmail.com>
2021-04-09 14:21:15 +0300

    Добавила тестирование сортировки, а также skip и limit

--- test/reading_test.js
+++ test/reading_test.js
@@ -2,11 +2,17 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading users out of database", () => {
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach((done) => {
     joe = new User({ name: "Joe" });
-    joe.save().then(() => done());
+    maria = new User({ name: "Maria" });
+    alex = new User({ name: "Alex" });
+    zach = new User({ name: "Zach" });
+
+    Promise.all([joe.save(), maria.save(), alex.save(), zach.save()]).then(() =>
+      done()
+    );
   });
 
   it("finds all users with a name of Joe", (done) => {
@@ -21,5 +27,18 @@ describe("Reading users out of database", () => {
       assert(user.name === "Joe");
       done();
     });
+  });
+
+  it("can skip and limit the result set", (done) => {
+    User.find({})
+      .sort({ name: 1 }) // ascending order
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
+        done();
+      });
   });
 });
